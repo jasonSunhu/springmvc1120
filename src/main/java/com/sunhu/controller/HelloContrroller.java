5f05f2846187bfd2121c7a4f0aa3800b4a0d2b86
jasonSunhu <jason_sunhu@163.com>
2019-11-27 19:55:35 +0800

    log4j2配置 mybatis的配置使用log4j2 todo:${web:rootDir}问题待解决

--- src/main/java/com/sunhu/controller/HelloContrroller.java
+++ src/main/java/com/sunhu/controller/HelloContrroller.java
@@ -2,6 +2,8 @@ package com.sunhu.controller;
 
 import com.sunhu.entity.SysUser;
 import com.sunhu.dao.SysUserMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +17,21 @@ import java.util.UUID;
 @RequestMapping("/main")
 public class HelloContrroller {
 
+    private final static Logger logger = LogManager.getLogger(HelloContrroller.class);
+
     @Autowired
     private SysUserMapper sysUserMapper;
 
     @RequestMapping("/frist")
     public String helloWorld(){
+        logger.warn("start");
         SysUser user = new SysUser();
         user.setId(UUID.randomUUID().toString().replaceAll("-",""));
         user.setUsername("jack");
         user.setPassword("12344");
         user.setAge(21);
         sysUserMapper.insert(user);
+        logger.error("end");
         return "hello";
     }
 }
